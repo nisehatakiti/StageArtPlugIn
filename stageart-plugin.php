8396fe82eb1c9b46068d19dec1d6e5b82db33b6b
nisehatakiti <nisehatakiti@gmail.com>
2026-09-15 11:57:18 +0900

    Define StageArt as an AuthCore application

--- stageart-plugin.php
+++ stageart-plugin.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: StageArt PlugIn
  * Description: StageArt features for standalone WordPress sites.
- * Version: 0.1.0
+ * Version: 0.2.0
  * Requires at least: 6.0
  * Requires PHP: 8.0
  * Author: nisehatakiti
@@ -10,6 +10,15 @@
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: stageart-plugin
  * Domain Path: /languages
+ * AuthCore: application
+ * AuthCore Application Key: stageart
+ * AuthCore Application Name: StageArt
+ * AuthCore Application Version: 0.2.0
+ * AuthCore Application URI: https://github.com/nisehatakiti/StageArtPlugIn
+ * AuthCore Vendor: nisehatakiti
+ * AuthCore Vendor URI: https://github.com/nisehatakiti
+ * AuthCore Description: 舞台芸術団体向け公演・団体・チケット管理
+ * AuthCore Icon: dashicons-theater
  */
 
 declare(strict_types=1);
@@ -18,7 +27,7 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-define('STAGEART_PLUGIN_VERSION', '0.1.0');
+define('STAGEART_PLUGIN_VERSION', '0.2.0');
 define('STAGEART_PLUGIN_FILE', __FILE__);
 define('STAGEART_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('STAGEART_PLUGIN_URL', plugin_dir_url(__FILE__));
